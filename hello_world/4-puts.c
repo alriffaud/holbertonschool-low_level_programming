36b0b94138a6e36710f9695f65e5991b64282c9b
Alberto Riffaud <alriffaud@google.com>
2023-10-10 09:47:47 -0700

    Create file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 /**
- * This program prints exactly \"Programming is like building a multilingual puzzle, followed by a new line.
+ * This program prints exactly \"Programming is like building a multilingual 
+ * puzzle, followed by a new line.
  * Return: 0
  */
 int main(void)
