159e5608117c6634bba9c1d2a611900bc7a1eea4
Alberto Riffaud <alriffaud@google.com>
2023-10-11 07:45:09 -0700

    Create file

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+* main - Prints the lowercase alphabet in reverse order
+*
+* Description: Write a program that prints the lowercase alphabet in reverse,
+* followed by a new line.
+*
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 122 ; i <= 97 ; i--)
+		putchar((char)i);
+
+	putchar('\n');
+	return (0);
+}
