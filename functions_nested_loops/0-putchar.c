c0a3d9a3b2195a674f568e4d47fbb38aa763c299
Alberto Riffaud <alriffaud@google.com>
2023-10-11 10:24:08 -0700

    Create file

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,6 +2,19 @@
 #include "main.h"
 
 /**
+* main - prints _putchar
+*
+* Description: This program prints _putchar, followed by a new line.
+*
+* Return: 0
+*/
+
+int main(void)
+{
+        print_putchar();
+}
+
+/**
 * print_putchar - prints _putchar
 *
 * Description: This program prints _putchar, followed by a new line.
