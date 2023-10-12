99aac9143afadbee4e3a57e9736c80162dde3441
Alberto Riffaud <alriffaud@google.com>
2023-10-12 10:48:04 -0700

    Create file

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @a: First integer to sum.
+ * @b: Second integer to sum.
+ *
+ * Description: This function prints the 9 times table, starting with 0.
+ *
+ * Return: None.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -111,5 +111,17 @@ void jack_bauer(void);
  */
 void times_table(void);
 
+/**
+ * add - Adds two integers
+ * @a: First integer to sum.
+ * @b: Second integer to sum.
+ *
+ * Description: This function prints the 9 times table, starting with 0.
+ *
+ * Return: None.
+ */
+int add(int a, int b);
+
+
 
 #endif
