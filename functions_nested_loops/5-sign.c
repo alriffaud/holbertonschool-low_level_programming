ea8d7e1efb3892e776f8f481bf79846c7d6105b0
Alberto Riffaud <alriffaud@google.com>
2023-10-12 06:05:00 -0700

    Create file

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number.
+ * @c: This is the char variable that we want to check.
+ *
+ * This function that prints the sign of a number.
+ *
+ * Return: Returns 1 and prints + if n is greater than zero. Returns 0 and
+ * prints 0 if n is zero. Returns -1 and prints - if n is less than zero.
+ */
+int print_sign(int c)
+{
+	if (c >= 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (c == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -60,4 +60,15 @@ int _islower(int c);
  */
 int _isalpha(int c);
 
+/**
+ * print_sign - Prints the sign of a number.
+ * @c: This is the char variable that we want to check.
+ *
+ * This function that prints the sign of a number.
+ *
+ * Return: Returns 1 and prints + if n is greater than zero. Returns 0 and
+ * prints 0 if n is zero. Returns -1 and prints - if n is less than zero.
+ */
+int print_sign(int c);
+
 #endif
