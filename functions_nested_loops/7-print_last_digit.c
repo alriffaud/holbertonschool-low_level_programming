9a44e38d49e206b5ef67d408daa8ec77c87091a9
Alberto Riffaud <alriffaud@google.com>
2023-10-12 06:30:04 -0700

    Create file

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - last digit
+ * @n: Is an integer number
+ *
+ * Description: This program will print the last digit of the number stored
+ * in the variable n.
+ *
+ * Return: The last digit of the number.
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -81,5 +81,16 @@ int print_sign(int c);
  */
 int _abs(int c);
 
+/**
+ * print_last_digit - last digit
+ * @n: Is an integer number
+ *
+ * Description: This program will print the last digit of the number stored
+ * in the variable n.
+ *
+ * Return: The last digit of the number.
+ */
+int print_last_digit(int n);
+
 
 #endif
