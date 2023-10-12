f20f461e7aa8dc2ee1b310fca662aaf638b7ec73
Alberto Riffaud <alriffaud@google.com>
2023-10-12 05:26:51 -0700

    Create file

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -15,11 +15,11 @@ int _isalpha(int c)
 	{
 		return (0);
 	}
-	else	
+	else
 	{
 		if ((c >= 48 && c <= 57) || (c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 			return (1);
 		else
 			return (0);
-	}	
+	}
 }
