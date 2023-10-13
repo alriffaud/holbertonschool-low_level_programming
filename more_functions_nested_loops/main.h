d104030d11d29be8b01b80c4ddadc767a3cccc69
Alberto Riffaud <alriffaud@google.com>
2023-10-13 10:34:45 -0700

    Create file

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -101,5 +101,16 @@ void print_diagonal(int n);
  */
 void print_square(int size);
 
+/**
+ * Fizz_Buzz - Prints the result of the Fizz-Buzz test.
+ *
+ * This function prints the numbers from 1 to 100, followed by a new line.
+ * But for multiples of three print Fizz instead of the number and for the
+ * multiples of five print Buzz. For numbers which are multiples of both
+ * three and five print FizzBuzz.
+ *
+ * Return: None.
+ */
+void Fizz_Buzz(void);
 
 #endif
