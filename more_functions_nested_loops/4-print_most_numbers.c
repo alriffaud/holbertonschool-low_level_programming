10450c4ff2d8c963707787d29ddcb0cdc3789462
Alberto Riffaud <alriffaud@google.com>
2023-10-13 07:21:53 -0700

    Create file

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_most_numbers - Prints most numbers, from 0 to 9.
+*
+* Description: This function prints the numbers, from 0 to 9 (excepting
+* numbers 2 and 4)followed by a new line.
+*
+* * Return: 0
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48 ; i <= 57 ; i++)
+	{
+		if (i == 50 || i == 52)
+			continue;
+		_putchar((char)i);
+	}
+	_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -51,5 +51,15 @@ int mul(int a, int b);
 */
 void print_numbers(void);
 
+/**
+* print_most_numbers - Prints most numbers, from 0 to 9.
+*
+* Description: This function prints the numbers, from 0 to 9 (excepting
+* numbers 2 and 4)followed by a new line.
+*
+* * Return: 0
+*/
+void print_most_numbers(void);
+
 
 #endif
