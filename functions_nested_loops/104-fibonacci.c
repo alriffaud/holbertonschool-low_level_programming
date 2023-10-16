3b38cc0532f9caaba34d3941870f5baecfbe52ef
Alberto Riffaud <alriffaud@google.com>
2023-10-16 13:02:03 -0700

    Create file

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -23,11 +23,11 @@ int main(void)
 		{
 			fib2_part1 = (long double)fib2 / 100000000;
 			fib2_part2 = (long double)(fib2 - 100000000 * fib2_part1);
-			printf(", %.0f%.0f", fib2_part1, fib2_part2);
+			printf(", %.0Lf%.0Lf", fib2_part1, fib2_part2);
 		}
 		else
 		{
-			printf(", %.0f", fib2);
+			printf(", %.0Lf", fib2);
 		}
 	}
 	printf("\n");
