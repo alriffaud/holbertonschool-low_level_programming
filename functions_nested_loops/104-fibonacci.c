49c1641cb990cfbe3386c448841d2a3b5b29afb7
Alberto Riffaud <alriffaud@google.com>
2023-10-16 11:44:04 -0700

    Create file

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 /**
- * main - Prints the first 50 Fibonacci numbers.
+ * main - Prints the first 98 Fibonacci numbers.
  *
- * Description: This program prints the first 50 Fibonacci numbers, starting
+ * Description: This program prints the first 98 Fibonacci numbers, starting
  * with 1 and 2, followed by a new line.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	unsigned long int fib1 = 1, fib2 = 2, contador = 2, aux;
+	double fib1 = 1, fib2 = 2, aux;
+	int contador;
 
 	printf("1, 2");
-	while (contador < 98)
+	for (contador = 2; contador < 50; contador++)
 	{
 		aux = fib1;
 		fib1 = fib2;
 		fib2 = aux + fib2;
-		printf(", %lu", fib2);
-		contador++;
+		printf(", %ld", (long int)fib2);
+	}
+	for (contador = 50; contador < 98; contador++)
+	{
+		aux = fib1;
+		fib1 = fib2;
+		fib2 = aux + fib2;
+		printf(", %ld", (long int)(fib2 / 100000));
+		printf("%ld", (long int)(fib2 % 100000));
 	}
 	printf("\n");
 	return (0);
