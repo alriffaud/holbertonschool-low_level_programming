efb7776c7afca6bb6a6fe15dae37ee8460a275e7
Alberto Riffaud <alriffaud@google.com>
2023-10-16 06:37:41 -0700

    Create file

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -35,7 +35,7 @@ void print_times_table(int n)
 				{
 					_putchar(prod / 100 + '0');
 					_putchar(((prod / 10) % 10) + '0');
-					_putchar(prod % 100 + '0');
+					_putchar(prod % 10 + '0');
 				}
 				else if (col != 0)
 				{
