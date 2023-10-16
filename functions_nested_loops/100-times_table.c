41375c9599e3f063a3cc01266dee9a777fe7eb88
Alberto Riffaud <alriffaud@google.com>
2023-10-16 05:39:45 -0700

    Create file

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -26,23 +26,23 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(n / 10 + '0');
-					_putchar(n % 10 + '0');
+					_putchar(prod / 10 + '0');
+					_putchar(prod % 10 + '0');
 				}
 				else if (prod >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(n / 100 + '0');
-					_putchar(((n / 10) % 10) + '0');
-					_putchar(n % 100 + '0');
+					_putchar(prod / 100 + '0');
+					_putchar(((prod / 10) % 10) + '0');
+					_putchar(prod % 100 + '0');
 				}
 				else
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(n + '0');
+					_putchar(prod + '0');
 				}
 			}
 			_putchar('\n');
