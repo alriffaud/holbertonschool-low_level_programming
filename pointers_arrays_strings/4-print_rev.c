cdb77e18f346d5428be98eb60236ef88c03c09dc
Alberto Riffaud <alriffaud@google.com>
2023-10-18 05:42:08 -0700

    Create file

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _puts - This function prints a string in reverse order.
+ * _print_rev - This function prints a string in reverse order.
  * @s: Is the pointer to an string.
  *
  * Description: Prints a string in reverse order, followed by a new line,
  * to stdout.
  * Return: None.
  */
-void _puts(char *s)
+void _print_rev(char *s)
 {
 	int cant;
 
