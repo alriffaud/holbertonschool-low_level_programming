61ff28e9145fc7e62bce55847f6f8608986d08d0
Alberto Riffaud <alriffaud@google.com>
2023-10-18 07:50:36 -0700

    Create file

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,40 @@
 #include "main.h"
 /**
+ * swap_char - This function swaps two characters.
+ * @a: Is the first pointer to a character to swaps.
+ * @b: Is the second pointer to a character to swaps.
+ *
+ * Return: None
+ */
+void swap_char(char *a, char *b)
+{
+	char aux = *a;
+
+	*a = *b;
+	*b = aux;
+}
+
+/**
+ * _strlen - This function returns the length of a string.
+ * @s: Is the pointer to an string.
+ *
+ * Return: An integer whith the length of the string.
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+	char *c;
+
+	c = s;
+	while (*c != '\0')
+	{
+		c++;
+		count++;
+	}
+	return (count);
+}
+
+/**
  * rev_string - This function returns the reverses of a string.
  * @s: Is the pointer to an string.
  *
@@ -7,22 +42,13 @@
  */
 void rev_string(char *s)
 {
-	int count = 0;
-	char *c;
+	int count, mitad, i = 0;
 
-	while (*s != '\0')
+	count = _strlen(s) - 1;
+	mitad = count / 2;
+	while (i <= mitad)
 	{
-		s++;
-		count++;
+		swap_char(s[i], s[count - i]);
+		i++;
 	}
-	s--;
-	*(c + count) = '\0';
-	count--;
-	while (count >= 0)
-	{
-		*c = *s;
-		s--;
-		count--;
-	}
-	return (c);
 }
