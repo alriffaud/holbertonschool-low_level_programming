a191a38a57c73adf2c1c637da155e7becd56036b
Alberto Riffaud <alriffaud@google.com>
2023-10-18 04:49:50 -0700

    Create file

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - Updates the value of a points to 98.
+ * @n: Is the pointer to an integer.
+ *
+ * Return: None
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef _PUTCHAR_H_
+#define _PUTCHAR_H_
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
