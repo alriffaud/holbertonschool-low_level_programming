16e71ec413023d8d7f5ee213615c3f950ebbf390
Alberto Riffaud <alriffaud@google.com>
2023-10-18 04:57:43 -0700

    Create file

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -2,7 +2,7 @@
 /**
  * swap_int - This function swaps the values of two integers.
  * @a: Is the first pointer to an integer to swaps.
- * @a: Is the second pointer to an integer to swaps.
+ * @b: Is the second pointer to an integer to swaps.
  * Return: None
  */
 void swap_int(int *a, int *b)
