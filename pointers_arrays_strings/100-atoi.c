34ee1ef72633bb86da7e3c0545c6bb154d049471
Alberto Riffaud <alriffaud@google.com>
2023-10-18 16:28:47 -0700

    Create file

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -13,8 +13,7 @@ int _atoi(char *s)
 
 	while (*s != '\0')
 	{
-		if (start == 1 && *s != '-' && *s != '+' && *s != ' '
-		&& !(*s >= '0' && *s <= '9'))
+		if (start == 1 && !(*s >= '0' && *s <= '9'))
 			break;
 		if (*s == '-')
 		{
@@ -22,22 +21,22 @@ int _atoi(char *s)
 		}
 		if (*s >= '0' && *s <= '9')
 		{
-			if (num_dig > 0 && res < (INT_MAX - 20) / 10)
+			if (num_dig > 0 && res < (INT_MAX - 100) / 10)
 			{
-				res = 10 * res + (*s - '0');
+				res = 10 * sign * res + sign * (*s - '0');
 				num_dig++;
 				start = 1;
 			}
 			else if (num_dig == 0)
 			{
-				res = *s - '0';
+				res = sign * (s - '0');
 				num_dig = 1;
 				start = 1;
 			}
 		}
 		s++;
 	}
-	if (res < (INT_MAX - 20) / 10)
+	if (res < (INT_MAX - 100) / 10)
 		return (sign * res);
 	else
 		return (10 * sign * res + sign * (*s - '0'));
