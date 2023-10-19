fda11b6d111fce5efe7e4186ff311298a600e1a2
Alberto Riffaud <alriffaud@google.com>
2023-10-19 07:41:14 -0700

    Create file

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,32 +4,27 @@
  * @s1: Is the pointer to the first string.
  * @s2: Is the pointer to the second string.
  *
- * Return: 0 if the two strings are equal, -15 if s1 is lower than s2 and
- * 15 if s1 is grater than s2.
+ * Return: 0 if the two strings are equal, -1 if s1 is lower than s2 and
+ * 1 if s1 is grater than s2. It work exactly like strcmp.
  */
 int _strcmp(char *s1, char *s2)
 {
-	int res = 0;
-
-	while (*s1 != '\0' && *s2 != '\0' && res == 0)
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		if (*s1 == *s2)
-			res = 0;
-		else if (*s1 < *s2)
+		if (*s1 < *s2)
 		{
-			res = -15;
-			break;
+			return (-1);
 		}
-		else
+		else if (*s1 > *s2)
 		{
-			res = 15;
-			break;
+			return (1);
 		}
-
+		s1++;
+		s2++;
 	}
 	if (*s1 == '\0' && *s2 != '\0')
-		res = -15;
-	if (*s1 != '\0' && *s2 == '\0')
-		res = 15;
-	return (res);
+		return (-1);
+	else if (*s1 != '\0' && *s2 == '\0')
+		return (1);
+	return (0);
 }
