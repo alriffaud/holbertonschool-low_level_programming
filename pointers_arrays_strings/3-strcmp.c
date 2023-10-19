61892ac5ca9f3d8178416c3af2c61e4b5103c7ec
Alberto Riffaud <alriffaud@google.com>
2023-10-19 07:48:39 -0700

    Create file

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  * @s1: Is the pointer to the first string.
  * @s2: Is the pointer to the second string.
  *
- * Return: 0, -1 or 1.
+ * Return: 0, -13 or 13.
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -12,18 +12,18 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (*s1 < *s2)
 		{
-			return (-1);
+			return (-13);
 		}
 		else if (*s1 > *s2)
 		{
-			return (1);
+			return (13);
 		}
 		s1++;
 		s2++;
 	}
 	if (*s1 == '\0' && *s2 != '\0')
-		return (-1);
+		return (-13);
 	else if (*s1 != '\0' && *s2 == '\0')
-		return (1);
+		return (13);
 	return (0);
 }
