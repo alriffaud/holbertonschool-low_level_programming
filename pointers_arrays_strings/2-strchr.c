fd03ef69a405b750c793ed0b9f2234c1feab7485
Alberto Riffaud <alriffaud@google.com>
2023-10-20 07:31:29 -0700

    Create file

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -12,13 +12,16 @@
  */
 char *_strchr(char *s, char c)
 {
+	if (s == NULL)
+	{
+		return (NULL);
+	}
 	while (*s != '\0')
 	{
 		if (*s == c)
 			return (s);
 		s++;
 	}
-	*s = '\0';
-	return (s);
+	return (NULL);
 
 }
