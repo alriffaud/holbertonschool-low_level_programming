cde0f43a2211fb0b9fd499f23ede7dce2395babb
Alberto Riffaud <alriffaud@google.com>
2023-10-20 09:46:00 -0700

    Create file

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -18,17 +18,17 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		return (0);
 	}
-	for (i = 0; s[i] != '\0'; ++i)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j]; ++j)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				break;
 			}
-			if (accept[j] == '\0')
-				break;
 		}
+		if (accept[j] == '\0')
+			break;
 	}
 	return (i);
 
