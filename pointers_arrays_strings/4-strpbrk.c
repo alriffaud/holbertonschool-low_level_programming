753c1a6099b1c5e04a55c4f20aaa5e5879125463
Alberto Riffaud <alriffaud@google.com>
2023-10-20 10:28:04 -0700

    Create file

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * *_strpbrk - This function searches a string for any of a set of bytes.
+ * @s: Is the pointer to an string.
+ * @accept: Is the pointer to an another string.
+ *
+ * Description: This function locates the first occurrence in the string s of
+ * any of the bytes in the string accept.
+ *
+ * Return: A pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int j;
+
+	if (s == NULL || accept == NULL || *accept == '\0')
+	{
+		return (s);
+	}
+	while (*s != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (*s == accept[j])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (s);
+
+}
