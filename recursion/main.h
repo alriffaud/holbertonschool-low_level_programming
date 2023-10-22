27b320076874b3455388754f166f7df008595572
Alberto Riffaud <alriffaud@google.com>
2023-10-22 16:34:51 -0700

    Create file

---
+++ recursion/main.h
@@ -0,0 +1,14 @@
+#ifndef _PUTCHAR_H_
+#define _PUTCHAR_H_
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _puts_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
+
+#endif
