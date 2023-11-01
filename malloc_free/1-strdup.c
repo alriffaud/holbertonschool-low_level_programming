57682366432aff2bf61288ee428a344afa6e51ba
Alberto Riffaud <alriffaud@google.com>
2023-11-01 06:01:44 -0700

    Create file

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* *_strdup - This function  returns a pointer to a newly allocated space in
+* memory, which contains a copy of the string given as a parameter.
+* @str: It's a pointer to the string given as a parameter.
+*
+* Return: NULL if str = NULL, on success returns a pointer to the duplicated
+* string. It returns NULL if insufficient memory was available.
+*/
+char *_strdup(char *str)
+{
+	char *res;
+	unsigned int i, len = strlen(str);
+
+	if (str == NULL)
+		return (NULL);
+	res = malloc(len);
+	if (res == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		res[i] = str[i];
+	res[len] = '\0';
+	return (res);
+}
