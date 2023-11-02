e36435ec3dc9695c7c3831e4776bf1111a5cbcb5
Alberto Riffaud <alriffaud@google.com>
2023-11-02 12:21:52 -0700

    Create file

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,11 +10,14 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *res;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	res = malloc(nmemb * size);
 	if (res == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		*((char *)res + i) = 0;
 	return (res);
 }
