7ad40185264127e0479f70d4d374c01b283ef18e
Alberto Riffaud <alriffaud@google.com>
2023-11-06 04:48:20 -0800

    Create file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - This define a new type struct dog.
+ * @name: The name of the dog.
+ * @age: It's age.
+ * owner: It's owner
+ *
+ */
 struct dog
 {
 	char *name;
