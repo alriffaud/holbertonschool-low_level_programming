c65f98276bb6ae1a86e91d37d07c91dd146f2f1b
Alberto Riffaud <alriffaud@google.com>
2023-11-06 06:24:44 -0800

    Create file

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 /**
  * *new_dog - This function initialize a variable of type dog_t.
@@ -11,13 +12,30 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new = malloc(sizeof(struct dog));
+	dog_t *new_dog = malloc(sizeof(struct dog));
 
-	if (new == NULL)
+	if (name == NULL || owner == NULL)
 		return (NULL);
-	new->name = name;
-	new->age = age;
-	new->owner = owner;
-	return (new);
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+	new_dog->name = malloc(strlen(name) + 1);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(new_dog->name, name);
+	new_dog->age = age;
+	new_dog->owner = malloc(strlen(owner) + 1);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(new_dog->owner, owner);
+	return (new_dog);
 }
 
