bfebce092b877c0250a68438778bf82b099f7241
Alberto Riffaud <alriffaud@google.com>
2023-11-21 05:14:15 -0800

    Creation of README.md, lists.h and 0-print_list.c files.

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - This function prints all the elements of a list_t list.
+ * @h: It's a pointer to the first element of the list.
+ *
+ * Return: The number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t node_count = 0;
+
+	if (h == NULL)
+		return (-1);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		node_count++;
+	}
+	return (node_count);
+}
