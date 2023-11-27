21124526aafc8422de015155ac9f06677a4e595a
Alberto Riffaud <alriffaud@google.com>
2023-11-27 10:45:13 -0800

    Correction of test.

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,13 +14,13 @@ int main(int ac, char **av)
 	char buffer[1024];
 
 	if (ac != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 	umask(0);
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
