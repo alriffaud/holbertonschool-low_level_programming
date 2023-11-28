d03ccfb44decd376709815b12d8d99c149aa8df4
Alberto Riffaud <alriffaud@google.com>
2023-11-28 05:24:09 -0800

    Creation of README.md, hash_tables.h and 0-hash_table_create.c files.

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - This function creates a hash table.
+ * @size: It's the size of the array.
+ *
+ * Return: A pointer to the newly created hash table, or NULL if something
+ * went wrong.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+
+	if (new_table == NULL)
+		return (NULL);
+	new_table->array = malloc(sizeof(hash_node_t) * size);
+	if (new_table->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
+	new_table->size = size;
+	return (new_table);
+}
